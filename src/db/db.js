b07841e36b1d41742ce111f103881ccb135e7032
Jon Love <jon_w-l@hotmail.com>
2025-01-05 12:12:06 +0800

    Continue set up

---
+++ src/db/db.js
@@ -0,0 +1,47 @@
+import { Sequelize } from 'sequelize'
+
+const migrationsConfig = {
+  migrationStorage: 'sequelize',
+  migrationStorageTableName: 'migrations',
+}
+
+const db_config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    url: process.env.DEV_DB_URL,
+    ...migrationsConfig,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    db_url: process.env.DB_URL,
+    ...migrationsConfig,
+  },
+}
+
+const env = process.env.NODE_ENV || 'development'
+
+const config = db_config[env]
+
+export const dbURL = env === 'production' ? config.db_url : config.url
+
+export const sequelize = new Sequelize(dbURL)
+
+export const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('connected to the database')
+  } catch (err) {
+    console.log('failed to connect to the database', err)
+    process.exit(1)
+  }
+}
