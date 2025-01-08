74b545fde1b6047b851de7a566c131dbe7a3a4b4
Jon Love <jon_w-l@hotmail.com>
2025-01-08 13:31:17 +0800

    Make changes for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import sessionsRouter from './controllers/sessions.js'
 import gamesRouter from './controllers/games.js'
 import answersRouter from './controllers/answers.js'
 import middleware from './middleware.js'
+import path from 'path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const staticFilesPath = path.join(__dirname, '../dist')
 
 const app = express()
 
@@ -17,11 +23,16 @@ app.use(
     origin: true,
   })
 )
+app.use(express.static(staticFilesPath))
 app.use(middleware.requestLogger)
 
 app.use('/api/v1/games', gamesRouter)
 app.use('/api/v1/answers', answersRouter)
 app.use('/api/v1/sessions', sessionsRouter)
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(staticFilesPath, 'index.html'))
+})
 
 app.use(middleware.unknownEndpoint)
 
